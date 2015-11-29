61885d38515845768d43091c13f9504b9ac7e179
Ilya Reznikov <ril.xilus@gmail.com>
2015-11-29 19:48:26 +0300

    Simple window

--- genetic.rkt
+++ genetic.rkt
@@ -1,4 +1,4 @@
-#lang scheme/base
+#lang racket/gui
 
 (define MAX_POPULATION 50)      ; Population size
 (define MAX_GENERATION 400)      ; Generation to stop evolving at
@@ -20,12 +20,12 @@
         (if (= 0 num)
             (cons (- 1 (car list)) (cdr list))
             (cons (car list) (remove-item (cdr list) (- num 1)))))
-
+      
       (let ((remove-pos (random (length specimen))))
         (if (= (list-ref specimen remove-pos) 1)
             (remove-item specimen remove-pos)
             (remove-random-item specimen))))
-
+    
     ; Return price of the specimen if conditions are met, otherwise remove random item and check again
     (let ((st (stats specimen)))
       (if (or (and (<= (car st)  W)
@@ -60,9 +60,9 @@
                                                 (car likehoods)))
                         likehoods)
                   (cdr fitness-map))))
-
+      
       (loop population '() fitness-map))
-
+    
     ; Return list of count specimen with best fitness from the current population
     (define (take-best-specimens count)
       (define (loop sorted-pop head count)
@@ -70,7 +70,7 @@
             head
             (loop (cdr sorted-pop) (cons (car sorted-pop) head) (- count 1))))
       (loop (sort population (lambda (x y) (> (fitness x) (fitness y)))) '() count))
-
+    
     ; Generate new population from the current
     (define (new-generation likehoods new-population reset)
       ; Choose parents and create one new specimen from them
@@ -82,7 +82,7 @@
             (if (= 0 pos)
                 (reverse head)
                 (list-head (cdr lst) (- pos 1) (cons (car lst) head))))
-
+          
           ; Breed, but take one of the parents if the child is too weak
           (let* ((crossover-pos (random (+ (length father) 1)))
                  (child (mutation (append (list-head mother crossover-pos '()) (list-tail father crossover-pos))))
@@ -91,20 +91,20 @@
                  (father-fitness (fitness father)))
             (cond ((and (< child-fitness (max mother-fitness father-fitness)) (< (random) 0.9)) father)
                   (else child))))
-
+        
         ; Get parent by the likehoods
         (define (get-parent population likehoods likehood)
           (if (< likehood (car likehoods))
               (car population)
               (get-parent (cdr population) (cdr likehoods) likehood)))
-
+        
         ; Choose random parents by roulette-wheel selection, try a few times if parents are the same
         (let ((mother (get-parent population likehoods (random)))
               (father (get-parent population likehoods (random))))
           (if (or (not (equal? mother father)) (> iteration MAX_PARENT_ITERATION))
               (breed mother father)
               (new-specimen (+ iteration 1)))))
-
+      
       ; Generate a new population from an old one, or take a few of the best and restart, if reset flag is true
       (cond (reset
              (append (take-best-specimens ELITISM_COUNT) (generate-first-population (- MAX_POPULATION ELITISM_COUNT))))
@@ -112,7 +112,7 @@
              (append (take-best-specimens ELITISM_COUNT) new-population))
             (else
              (new-generation likehoods (cons (new-specimen 1) new-population) #f))))
-
+    
     ; Check if most of the population is of the similar fitness
     (define (check-dead-end fitness-map)
       (define (loop fitness-map cnt max delta)
@@ -124,9 +124,9 @@
                (loop (cdr fitness-map) (+ cnt 1) max delta))
               (else
                (loop (cdr fitness-map) cnt max delta))))
-
+      
       (loop fitness-map 0 (apply max fitness-map) (apply max costs)))
-
+    
     ; Do the evolution!
     (let* ((fitness-map (map fitness population))
            (total-fitness (foldl + 0 fitness-map)))
@@ -142,12 +142,12 @@
     ; Generate random specimen
     (define (generate-specimen)
       (build-list (length weights) (lambda (x) (random 2))))
-
+    
     (define (loop N population)
       (if (= 0 N)
           population
           (loop (- N 1) (cons (generate-specimen) population))))
-
+    
     (loop N '()))
   
   ; Find the best specimen that fits the conditions
@@ -183,9 +183,21 @@
             (else
              (loop (cdr specimen) (+ N 1) result))))
     (loop specimen 1 '()))
-        
+  
+  ; Do some gui stuff
+  (define frame (new frame% [label "Genetic"] [width 300] [height 300]))
+  (define msg (new message% [parent frame]
+                   [label "Calculating solution..."]))
+  
+  
   ; Generate first ever population ant let them do their job
-  (let ((best-specimen (find-best-specimen (evolve (generate-first-population MAX_POPULATION) 1 1))))
-    (if (null? best-specimen)
-        '(#f)
-        (cons #t (append (stats best-specimen) (list (convert-specimen best-specimen)))))))
+  (send frame show #t)
+  (send msg set-label "Calculating solution...")
+  (let* ((best-specimen (find-best-specimen (evolve (generate-first-population MAX_POPULATION) 1 1)))
+         (result (if (null? best-specimen)
+                     '(#f)
+                     (cons #t (append (stats best-specimen) (list (convert-specimen best-specimen)))))))
+    (if (eq? #f (car result))
+        (send msg set-label "No solution")
+        (send msg set-label "Solution found"))
+    result))
